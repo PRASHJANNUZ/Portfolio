f11e241ebacf316f9c67e79690ec55975c7ce6c7
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2024-02-27 13:08:07 +0530

    file added

---
+++ src/data/constants.js
@@ -0,0 +1,268 @@
+export const Bio = {
+  name: "Prashant Jannu",
+  roles: [
+    "Java Developer...",
+    "Software Engineer...",
+    "Programmer...",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/PRASHJANNUZ",
+  resume:
+    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/prashant-jannu-966414274",
+  twitter: " ",
+  insta: "https://www.instagram.com/prashant_jannuz_08/",
+  facebook: " ",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      {
+        name: "Spring",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuOIR8TBB2mwOB3UR3Pon0gineKJrRLEYHT-vaZQlITg&s",
+      },
+      {
+        name: "Hibernate",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/hibernate.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://static-00.iconduck.com/assets.00/sql-database-sql-azure-icon-1955x2048-4pmty46t.png",
+      },
+      {
+        name: "SpringBoot",
+        image:
+          "https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png",
+      },
+      {
+        name: "Rest Api",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1400/1*-dbPsi8Mdqj5Y0454eGvPQ.png",
+      },
+  
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Spring Tool Suite",
+        image:
+          "https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+
+    ],
+  },
+  {
+    title: "Servers",
+    skills: [
+      
+      {
+        name: "Apache Tomcat",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9TFbqPCc8rnID7pL9sE881R1Kuob0dD6TKln7eOgYUA&s",
+      },
+      {
+        name: "kafka",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVuVnwImDVM4-bPMDT-e3EXuZiCrinbV9kWtl9rL0-Ow&s",
+      },
+
+
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.vcacs.ac.in/assets/images/logo/white-logo.png",
+    school: "Vishwakarma College of Arts Commerce and Science",
+    date: "Sep 2023 - Apr 2025",
+    grade: "8.18 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer Science at Vishwakarma College of Arts Commerce and Science, Pune , I have completed 1 semesters and have a CGPA of 8.18 ",
+    degree: "Master's of Computer science - Msc",
+  },
+
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEw5NcrtjOBQLPWHXQe6LKF0DPyNMadcyECDPXeZJQPTIUrmFY4tKNkqjt9wfQnsJ9NdQ&usqp=CAU",
+    school: "S.B.B alias Appasaheb Jedhe Arts, Commerce & Science College, Pune",
+    date: "Aug 2020 - Apr 2023",
+    grade: "8.45 CGPA",
+    desc: "I have completed my Bachelor's degree at S.B.B alias Appasaheb Jedhe Arts, Commerce & Science College, Pune , where I studied Science with Computer Science.",
+    degree: "Bachelor's of Computer science - Bsc(Computer Scienece)",
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D4D0BAQESEsfUsfEvag/company-logo_200_200/0/1697364408308?e=2147483647&v=beta&t=KSnrCLn2cDMegf-GVqxnUGTd3nOojmgikLX_9m-6NkE",
+    role: "Backend Developer Intern",
+    company: "Novizit",
+    date: "Nov 2023 - jan 2024",
+    desc: "Description :- Build some Rest Api for buisness services",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "Hibernate",
+      "Rest Api",
+      "PostMan",
+      "Microservices",
+    ],
+    // doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+
+
+];
+
+
+
+export const projects = [
+ 
+  {
+    id: 3,
+    title: "Real Estate Buissness",
+    date: "Feb 2023 - Apr 2023",
+    description:
+      "In this project I have build a web Application. Where User Can Sell the property by uploading all the details on website, And can also Buy the property as well!",
+    image:
+      "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600",
+    tags: ["Java", "Springboot", "Servlets & JSP", "Hibernate", "Html" , "CSS" , "Tomcat"],
+    category: "web app",
+    github: "https://github.com/PRASHJANNUZ?tab=repositories",
+    //webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Expense Tracking System",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "In this project I have build a web Service, which tracks all the Expenses of user!",
+    image:
+      "https://images.pexels.com/photos/730547/pexels-photo-730547.jpeg?auto=compress&cs=tinysrgb&w=600",
+    tags: ["Springboot", "Java", "Rest Apis" , "Postman", "JSON" , "MySQL"],
+    category: "Web services",
+    github: "https://github.com/PRASHJANNUZ/IntershipTaskRW",
+   // webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+
+  {
+    id: 4,
+    title: "Hotel Rating System",
+    date: "Aug 2023 - sep 2023",
+    description:
+      "In this project I have build a Microservice, which tracks all the Hotel Ratings of user! it Consist of 3 different services like Hotel, User & Rating! ",
+    image:
+      "https://images.pexels.com/photos/827528/pexels-photo-827528.jpeg?auto=compress&cs=tinysrgb&w=600",
+    tags: ["Springboot", "Java", "Microservices", "Rest Apis" , "Postman", "JSON" ,"MySQL"],
+    category: "Web services",
+    github: "https://github.com/PRASHJANNUZ/microservices",
+   // webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+
+
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
